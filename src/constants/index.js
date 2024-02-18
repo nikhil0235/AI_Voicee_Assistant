2b5e374ba5f85a077a68808863c7473e5d172c52
nikhil0235 <rajnikhil705@gmail.com>
2024-02-18 12:30:13 +0530

    first-commit

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+export const apiKey = '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';
+
+export const dummyMessages = [
+    {
+        role: 'user', 
+        content: 'How are you?'
+    },
+    {
+        role: 'assistant',
+        content: "I'm fine, How may i help you today."
+    },
+    {
+        role: 'user',
+        content: 'create an image of a dog playing with cat'
+    },
+    {
+        role: 'assistant',
+        content: 'https://storage.googleapis.com/pai-images/ae74b3002bfe4b538493ca7aedb6a300.jpeg'
+    }
+]
